d55c10894dc41bb16fc14d02e7faac8dfbc760f2
NovelScript5 <hasansekercioglu03@gmail.com>
2023-10-25 17:32:30 +0300

    first commit

---
+++ dist/src/menu.js
@@ -0,0 +1,89 @@
+export function menu() {
+
+    let mainhtml = `
+    
+    <section class="menuSection">
+
+<h1>Menu</h1>
+
+<div class="menuwrapper">
+
+<div class="menuitem">
+
+<img src="Images/1.jpg" alt="">
+<div>
+
+<div class="name-price">
+
+<p>Pasta:</p>
+<p>$10</p>
+
+</div>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolore dicta facere</p>
+
+</div>
+
+</div>
+
+<div class="menuitem">
+
+<img src="Images/2.jpg" alt="">
+<div>
+    
+<div class="name-price">
+    
+<p>Revani:</p>
+<p>$20</p>
+    
+</div>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolore dicta facere</p>
+    
+</div>
+    
+</div>
+
+<div class="menuitem">
+
+<img src="Images/3.jpg" alt="">
+<div>
+        
+<div class="name-price">
+        
+<p>Sote:</p>
+<p>$30</p>
+        
+</div>
+        
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolore dicta facere</p>
+        
+</div>
+        
+</div>
+
+<div class="menuitem">
+
+<img src="Images/4.jpg" alt="">
+<div>
+            
+<div class="name-price">
+            
+<p>Soup:</p>
+<p>$50</p>
+            
+</div>
+            
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus dolore dicta facere</p>
+            
+</div>
+            
+</div>
+
+</div>
+
+</section>
+    `
+
+    return mainhtml
+    
+    }
